8d29edd6762d1c94941c9190f662cd5f5a5f90ee
Acerkoo <747985742@qq.com>
2020-03-30 15:30:18 +0800

    init

---
+++ src/test/java/cn/watermelon/watermelonbackend/WatermelonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.watermelon.watermelonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermelonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
