e84b386585251534f04fddb92aae71314036ca22
张愉飞 <>
2020-04-01 21:54:12 +0800

    Problem solve

--- src/main/java/cn/watermelon/watermelonbackend/WatermelonBackendApplication.java
+++ src/main/java/cn/watermelon/watermelonbackend/WatermelonBackendApplication.java
@@ -1,9 +1,14 @@
 package cn.watermelon.watermelonbackend;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@MapperScan("cn.watermelon.watermelonbackend.mapper")
+
 public class WatermelonBackendApplication {
 
     public static void main(String[] args) {
