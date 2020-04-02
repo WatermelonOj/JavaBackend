ef81043a04ebf6233f09c262da7ff764656c07c9
张愉飞 <>
2020-04-02 20:42:38 +0800

    Finish problem function

---
+++ src/main/java/cn/watermelon/watermelonbackend/service/impl/ProblemServiceImpl.java
@@ -0,0 +1,30 @@
+package cn.watermelon.watermelonbackend.service.impl;
+
+import cn.watermelon.watermelonbackend.entity.Problem;
+import cn.watermelon.watermelonbackend.mapper.ProblemMapper;
+import cn.watermelon.watermelonbackend.service.ProblemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProblemServiceImpl implements ProblemService {
+    @Autowired
+    private ProblemMapper problemMapper;
+    @Override
+    public List<Problem> findAll()
+    {
+        return problemMapper.findAll();
+    }
+    @Override
+    public List<Problem> findProblemByName(String name)
+    {
+        return problemMapper.findProblemByName(name);
+    }
+    public List<Problem> getFindProblemByName(int id)
+    {
+        return problemMapper.findProblemById(id);
+    }
+
+}
