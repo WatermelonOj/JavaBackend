b7c49dd0c9ad7e1172cf8f5d15357ae11ea3ca46
张愉飞 <>
2020-04-08 20:25:44 +0800

    Fix Server problem

---
+++ src/main/resources/sql/init/problem.sql
@@ -0,0 +1,4 @@
+create table if not exists 'problem'(	problem_id int primary key,title varchar(255) ,	description varchar(255) ,
+intput varchar(255) ,output varchar(255) ,	sample_input varchar(255) ,sample_output varchar(255) ,
+hint varchar(255) ,spj bit ,contest_id varchar(255) ,visible bit,tm_limit varchar(255) ,mem_limit varchar(255));
+
