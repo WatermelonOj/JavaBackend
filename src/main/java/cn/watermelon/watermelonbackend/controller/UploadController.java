1764d50b2c2be5c25bb95ad57446aee07ba35835
张愉飞 <>
2020-05-26 17:56:40 +0800

    添加上传，启用Redis

--- src/main/java/cn/watermelon/watermelonbackend/controller/UploadController.java
+++ src/main/java/cn/watermelon/watermelonbackend/controller/UploadController.java
@@ -12,10 +12,34 @@ import java.util.Map;
 public class UploadController {
 
     @RequestMapping(value = "/uploadfile/{filename}", method = RequestMethod.POST)
-    public boolean uploadImg(@PathVariable("filename") String fileName,
+    public boolean uploadInputAndOutput(@PathVariable("filename") String fileName,
                                         @RequestParam("file") MultipartFile file,@RequestParam("type") String type,@RequestParam("id") int id) {
         if(file != null) {
-            String filePath = "~/problem/" + Integer.toString(id) + "/" + type;
+            String filePath = "../problem/" + Integer.toString(id) + "/" + type;
+            if (Upload.getInstance().uploadFileInPath(file, fileName, filePath)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @RequestMapping(value = "/uploadimg/{filename}",method = RequestMethod.POST)
+    public boolean uploadUserImg(@PathVariable("filename") String fileName, @RequestParam("file") MultipartFile file ,@RequestParam("id") int id)
+    {
+        if(file != null)
+        {
+            String filePath = "../Img/" + Integer.toString(id) + "/";
+            if (Upload.getInstance().uploadFileInPath(file, fileName, filePath)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @RequestMapping(value = "/uploadbackground/{filename}",method = RequestMethod.POST)
+    public boolean uploadUserBackGroud(@PathVariable("filename") String fileName, @RequestParam("file") MultipartFile file ,@RequestParam("id") int id)
+    {
+        if(file != null)
+        {
+            String filePath = "../BackGround/" + Integer.toString(id) + "/";
             if (Upload.getInstance().uploadFileInPath(file, fileName, filePath)) {
                 return true;
             }
